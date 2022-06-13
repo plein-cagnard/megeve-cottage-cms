4f7dbf1546c4fa4aa046b3b4b84e36c14c272e89
arthur <arthureuw@gmail.com>
2022-06-13 15:51:23 +0200

    update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -17,15 +17,3 @@ module.exports = ({ env }) => ({
     debug: false,
   },
 });
-
-// const path = require('path');
-//
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
