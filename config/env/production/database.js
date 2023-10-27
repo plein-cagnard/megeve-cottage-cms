51c623e10a0d755a52ca280bce381571b6b2b361
arthur <arthureuw@gmail.com>
2023-10-27 14:04:17 +0200

    update dbK

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: 'db',
-      port: 3306,
-      database: 'strapi',
-      user: 'strapi',
-      password: 'strapi',
+      host: env('DATABASE_HOST', 'db'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
