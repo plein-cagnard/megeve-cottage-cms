1b1f1ec30f5a4a812e981f7150f18f8be04302c8
arthur <arthureuw@gmail.com>
2023-10-27 13:17:54 +0200

    update db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'db'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'tqnc1114_megeve-cottage'),
-      user: env('DATABASE_USERNAME', 'tqnc1114_react-live'),
-      password: env('DATABASE_PASSWORD', 'iAjneWF6LqH3AZBaTcBRtSN9vmSaEi6NSgsXxfynfBBAAV5zMY'),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
